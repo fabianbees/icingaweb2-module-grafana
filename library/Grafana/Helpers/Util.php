7b4c73ac78eb7ea6591c883b0c85f4417f6e199e
Markus Opolka <markus.opolka@netways.de>
2024-12-04 11:49:49 +0100

    Remove some unused code

--- library/Grafana/Helpers/Util.php
+++ library/Grafana/Helpers/Util.php
@@ -4,63 +4,10 @@ namespace Icinga\Module\Grafana\Helpers;
 
 class Util
 {
-
     public static function graphiteReplace(string $string = ''): string
     {
         $string = preg_replace('/[^a-zA-Z0-9\*\-:]/', '_', $string);
 
         return $string;
-    }
-
-    public static function httpStatusCodetoString(int $code = 0): string
-    {
-        $statuscodes = [
-            '100' => 'Continue',
-            '101' => 'Switching Protocols',
-            '200' => 'OK',
-            '201' => 'Created',
-            '202' => 'Accepted',
-            '203' => 'Non-Authoritative Information',
-            '204' => 'No Content',
-            '205' => 'Reset Content',
-            '206' => 'Partial Content',
-            '300' => 'Multiple Choices',
-            '302' => 'Found',
-            '303' => 'See Other',
-            '304' => 'Not Modified',
-            '305' => 'Use Proxy',
-            '400' => 'Bad Request',
-            '401' => 'Unauthorized',
-            '402' => 'Payment Required',
-            '403' => 'Forbidden',
-            '404' => 'Not Found',
-            '405' => 'Method Not Allowed',
-            '406' => 'Not Acceptable',
-            '407' => 'Proxy Authentication Required',
-            '408' => 'Request Timeout',
-            '409' => 'Conflict',
-            '410' => 'Gone',
-            '411' => 'Length Required',
-            '412' => 'Precondition Failed',
-            '413' => 'Request Entity Too Large',
-            '414' => 'Request-URI Too Long',
-            '415' => 'Unsupported Media Type',
-            '416' => 'Requested Range Not Satisfiable',
-            '417' => 'Expectation Failed',
-            '500' => 'Internal Server Error',
-            '501' => 'Not Implemented',
-            '502' => 'Bad Gateway',
-            '503' => 'Service Unavailable',
-            '504' => 'Gateway Timeout',
-            '505' => 'HTTP Version Not Supported'
-        ];
-
-        $code = (string)$code;
-
-        if (array_key_exists($code, $statuscodes)) {
-            return $statuscodes[$code];
-        }
-
-        return $code;
     }
 }
